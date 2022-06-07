6bdd09df3c7e43fb8530136fce690acf382d4a6a
WONYOUNGHUB <zero2317@naver.com>
2022-06-07 12:33:52 +0900

    로그아웃 기능 일단구현

---
+++ src/main/java/com/kosta/project/member/controller/MemberLogoutController.java
@@ -0,0 +1,30 @@
+package com.kosta.project.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MemberLogoutController
+ */
+@WebServlet("/member/logout")
+public class MemberLogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		HttpSession session = request.getSession(false);
+		if(session != null)
+			session.invalidate();
+	     request.getRequestDispatcher("/jsp/login.jsp")
+         .forward(request, response);   
+		
+	}
+}
