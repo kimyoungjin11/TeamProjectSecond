fad974c16c5424f538efda2bf69b01a4dead27cb
pineyun <findfinepine@gmail.com>
2022-06-09 11:29:28 +0900

    commit

--- src/main/java/com/kosta/project/productInsert/ProductInsertServlet.java
+++ src/main/java/com/kosta/project/productInsert/ProductInsertServlet.java
@@ -41,55 +41,39 @@ public class ProductInsertServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8"); //한글 인코딩
-		Product product = productInsert(request);
+		Product product =  new Product();
 		ProductService pService = new ProductService();
-
 		String dir = request.getServletContext().getRealPath(UPLOAD_DIR);
 		System.out.println("웹서버 경로 : " + dir);
 		Map<String, Object> map = UploadFileHelper.uploadFile(UPLOAD_DIR, request);
+		Map<String, String> mapParams = (Map<String, String>) map.get("params");
+		for(String key:mapParams.keySet()) {
+			if(key.equals("category_id")) product.setCategory(Integer.parseInt(mapParams.get("category_id")));
+			if(key.equals("price")) product.setPrice(Integer.parseInt(mapParams.get("price")));
+			if(key.equals("join_number")) product.setJoinNumber(Integer.parseInt(mapParams.get("join_number")));
+			 
+			if(key.equals("title")) product.setproductTitle(mapParams.get("title"));
+			if(key.equals("content")) product.setproductContent(mapParams.get("content"));
+		}
+		HttpSession session = request.getSession();
+		//Member member = (Member)session.getAttribute("loginMember");
+		Member member = new Member(); member.setUserId("admin"); //윗줄과 trade
+		product.setUserId(member.getUserId());
+		System.out.println(product);
 		
-				
-		int result = pService.productInsert(product);
+		int result = pService.productInsert(product);//product table insert
+		int pid = pService.maxProductNO();
+		List<String> imageList = (List<String>)map.get("photos");
+		int result2 = pService.inserProductImages(pid, imageList);
 		
-			
 		request.setAttribute("message", result>0? "게시 성공" : "게시 실패");
 		RequestDispatcher rd;
 		rd = request.getRequestDispatcher("index.jsp");
 		rd.forward(request, response);
 	
 	}
+ 
 	
-	private Product productInsert(HttpServletRequest request) {
-		Product product = new Product();
-		
-		int categoryId = readInt(request, "category_id");
-		int price = readInt(request, "price");
-		int join_number = readInt(request, "join_number");
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-	
-		HttpSession session = request.getSession();
-		Member member = (Member)session.getAttribute("loginMember");
-		/*
-		 * Member member = new Member(); member.setUserId("admin");
-		 */
-		System.out.println(member);
-		product.setUserId(member.getUserId());
-		product.setCategory(categoryId);
-		product.setPrice(price);
-		product.setJoinNumber(join_number);
-		product.setproductTitle(title);
-		product.setproductContent(content);
-		
-		
-		System.out.println(product);
-		return product;
-	}
-	
-	private int readInt(HttpServletRequest request, String column) {
-		String data = request.getParameter(column);
-		return Integer.parseInt(data);
-	}
-	
+	 
 
 }
